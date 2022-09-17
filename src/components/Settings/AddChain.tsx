732480594426454ef7bb704c908fe1bb5a629068
CodinMaster <latheyapoorv@gmail.com>
2022-09-18 03:49:14 +0530

    auto-fetch chainId from RPC

--- src/components/Settings/AddChain.tsx
+++ src/components/Settings/AddChain.tsx
@@ -13,6 +13,7 @@ import {
 } from "@chakra-ui/react";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 import { useNetworks } from "@/contexts/NetworksContext";
+import { StaticJsonRpcProvider } from "@ethersproject/providers";
 
 function AddChain({ back }: { back: () => void }) {
   const { networksInfo, setNetworksInfo } = useNetworks();
@@ -20,11 +21,11 @@ function AddChain({ back }: { back: () => void }) {
   const [chainName, setChainName] = useState<string>();
   const [chainId, setChainId] = useState<string>();
   const [rpc, setRpc] = useState<string>();
-  const [isAdding, setIsAdding] = useState(false);
+  const [isBtnLoading, setIsBtnLoading] = useState(false);
   const [isChainNameNotUnique, setIsChainNameNotUnique] = useState(false);
 
   const addChain = () => {
-    setIsAdding(true);
+    setIsBtnLoading(true);
 
     if (chainName && chainId && rpc && networksInfo) {
       if (networksInfo[chainName]) {
@@ -43,7 +44,7 @@ function AddChain({ back }: { back: () => void }) {
       }
     }
 
-    setIsAdding(false);
+    setIsBtnLoading(false);
   };
 
   return (
@@ -76,6 +77,37 @@ function AddChain({ back }: { back: () => void }) {
             isInvalid={isChainNameNotUnique}
           />
           <Input
+            placeholder="RPC Url"
+            aria-label="RPC Url"
+            autoComplete="off"
+            minW="20rem"
+            size="sm"
+            rounded="lg"
+            value={rpc}
+            onChange={(e) => {
+              setRpc(e.target.value);
+            }}
+            onPaste={async (e) => {
+              // auto-fetch chainId from rpc url
+              setIsBtnLoading(true);
+              try {
+                const items = e.clipboardData.items;
+
+                for (const index in items) {
+                  const item = items[index];
+
+                  if (item.kind === "string" && item.type === "text/plain") {
+                    const _rpc = e.clipboardData.getData("Text");
+                    const provider = new StaticJsonRpcProvider(_rpc);
+                    const _chainId = (await provider.getNetwork()).chainId;
+                    setChainId(_chainId.toString());
+                  }
+                }
+              } catch (e) {}
+              setIsBtnLoading(false);
+            }}
+          />
+          <Input
             placeholder="Chain Id"
             aria-label="Chain Id"
             type="number"
@@ -88,25 +120,13 @@ function AddChain({ back }: { back: () => void }) {
               setChainId(e.target.value);
             }}
           />
-          <Input
-            placeholder="RPC Url"
-            aria-label="RPC Url"
-            autoComplete="off"
-            minW="20rem"
-            size="sm"
-            rounded="lg"
-            value={rpc}
-            onChange={(e) => {
-              setRpc(e.target.value);
-            }}
-          />
           <Center>
             <Button
               size="sm"
               maxW="6rem"
               colorScheme="blue"
               onClick={() => addChain()}
-              isLoading={isAdding}
+              isLoading={isBtnLoading}
             >
               Add Chain
             </Button>
