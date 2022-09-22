6ad3ac0cdc2835718481849b7cf06748d735fcd7
CodinMaster <latheyapoorv@gmail.com>
2022-09-22 17:41:57 +0530

    add credits footer

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,9 +1,20 @@
+import { HStack, Text, Link } from "@chakra-ui/react";
 import Chains from "./Chains";
 
 function Settings({ close }: { close: () => void }) {
   return (
     <>
       <Chains close={close} />
+      <HStack>
+        <Text>Built by</Text>
+        <Link
+          onClick={() => {
+            chrome.tabs.create({ url: "https://twitter.com/apoorvlathey" });
+          }}
+        >
+          @apoorvlathey
+        </Link>
+      </HStack>
     </>
   );
 }
